a3333b0a8e99f5da0709ac21a93a43ba1f04851c
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-02-10 14:25:04 +0300

    feat(guards): added auth and admin guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
+import { EmptyComponent } from './features/pages/empty/empty.component';
+import { FeedComponent } from './features/pages/feed/feed.component';
 import { ProfileComponent } from './features/pages/profile/profile.component';
 import { UsersComponent } from './features/pages/users/users.component';
-import { FeedComponent } from './features/pages/feed/feed.component';
-import { EmptyComponent } from './features/pages/empty/empty.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'feed', pathMatch: 'full' },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
   { path: 'users', component: UsersComponent },
   { path: 'feed', component: FeedComponent },
   { path: '**', component: EmptyComponent },

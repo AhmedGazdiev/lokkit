e49e4c402c8a09eb80e4056fd621f7c04abd4b33
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-02-11 13:30:48 +0300

    fix(layout): fixed global layout

--- src/app/shared/components/feed-layout/feed-layout.component.ts
+++ src/app/shared/components/feed-layout/feed-layout.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { FeedComponent } from '../../../features/pages/feed/feed.component';
-import { SidebarComponent } from '../sidebar/sidebar.component';
 import { SideRightComponent } from '../side-right/side-right.component';
 
 @Component({
   selector: 'app-feed-layout',
-  imports: [FeedComponent, SidebarComponent, SideRightComponent],
+  imports: [FeedComponent, SideRightComponent],
   templateUrl: './feed-layout.component.html',
   styleUrl: './feed-layout.component.scss',
 })

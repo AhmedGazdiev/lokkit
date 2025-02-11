4394cff58e9c07bd3afa42650c2d73da77d49eb0
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-02-11 11:53:37 +0300

    feat(sideright): added right side component

--- src/app/shared/components/feed-layout/feed-layout.component.ts
+++ src/app/shared/components/feed-layout/feed-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FeedComponent } from '../../../features/pages/feed/feed.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { SideRightComponent } from '../side-right/side-right.component';
 
 @Component({
   selector: 'app-feed-layout',
-  imports: [FeedComponent, SidebarComponent],
+  imports: [FeedComponent, SidebarComponent, SideRightComponent],
   templateUrl: './feed-layout.component.html',
   styleUrl: './feed-layout.component.scss',
 })
---
+++ src/app/shared/components/side-right/side-right.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { IconComponent } from '../icon/icon.component';
+
+@Component({
+  selector: 'app-side-right',
+  imports: [IconComponent],
+  templateUrl: './side-right.component.html',
+  styleUrl: './side-right.component.scss',
+})
+export class SideRightComponent {}

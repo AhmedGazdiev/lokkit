a835da19a51e201d144b8cde2f8ba02963561b6b
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-02-18 09:26:25 +0300

    feat(login form): finished login form

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -7,7 +7,6 @@ export class AuthService {
   public isLoggedIn: boolean = false;
 
   public login(email: any, password: any) {
-    this.isLoggedIn = true;
     console.log('Данные пользователя:', email, password)
   }
 }

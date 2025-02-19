808a9b178705bdd54d5ce0ab816261440aa60f46
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-02-19 17:44:06 +0300

    fix(styles): fixed styles

--- src/app/features/Post/components/create-post-form/create-post-form.component.ts
+++ src/app/features/Post/components/create-post-form/create-post-form.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {IconComponent} from '../../../../shared/components/icon/icon.component';
+import {InputComponent} from '../../../../shared/components/input/input.component';
 
 @Component({
   selector: 'app-create-post-form',
-  imports: [],
+  imports: [
+    FormsModule,
+    IconComponent,
+    ReactiveFormsModule,InputComponent
+  ],
   templateUrl: './create-post-form.component.html',
   styleUrl: './create-post-form.component.scss'
 })

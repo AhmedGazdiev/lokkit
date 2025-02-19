5f19855110dc1287d20a20168361760a01c573e0
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-02-19 22:55:40 +0300

    fix(suggestions): fixed

--- src/app/shared/components/suggestions/suggestions.component.ts
+++ src/app/shared/components/suggestions/suggestions.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IconComponent } from '../icon/icon.component';
 
 @Component({
-  selector: 'app-side-right',
+  selector: 'app-suggestions',
   imports: [IconComponent],
   templateUrl: './suggestions.component.html',
   styleUrl: './suggestions.component.scss',

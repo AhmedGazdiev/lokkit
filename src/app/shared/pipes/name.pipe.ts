c2d4f675cdeea1b59b79d764ba56f716549f1e49
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-02-20 21:11:10 +0300

    feat(pipe): added names format pipe

---
+++ src/app/shared/pipes/name.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {User} from '../../core/models/user';
+
+@Pipe({
+  name: 'name'
+})
+export class NamePipe implements PipeTransform {
+
+  transform(value: User): string {
+    return value.name + ' ' + value.username;
+  }
+
+}

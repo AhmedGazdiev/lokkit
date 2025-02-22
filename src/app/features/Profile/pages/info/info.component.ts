b0fa75b4e03622fd21642a305d00c4fd953cc22d
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-02-22 18:21:55 +0300

    feat(update user): added an info-form

--- src/app/features/Profile/pages/info/info.component.ts
+++ src/app/features/Profile/pages/info/info.component.ts
@@ -1,10 +1,11 @@
 import {Component, inject} from '@angular/core';
 import {UserService} from '../../../../core/services/user.service';
 import {InputComponent} from '../../../../shared/components/input/input.component';
+import {InfoFormComponent} from '../../components/info-form/info-form.component';
 
 @Component({
   selector: 'app-info',
-  imports: [InputComponent],
+  imports: [InputComponent, InfoFormComponent],
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss',
 })

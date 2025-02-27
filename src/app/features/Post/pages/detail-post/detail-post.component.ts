086df5d1b3173afdecfa4e4e00fddea8dfc1f033
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-02-27 16:57:42 +0300

    feat(detail-post): added detail-post functionality

--- src/app/features/Post/pages/detail-post/detail-post.component.ts
+++ src/app/features/Post/pages/detail-post/detail-post.component.ts
@@ -1,11 +1,22 @@
-import {Component} from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from '../../../../core/models/post';
+import { PostService } from '../../../../core/services/post.service';
 
 @Component({
   selector: 'app-detail-post',
   imports: [],
   templateUrl: './detail-post.component.html',
-  styleUrl: './detail-post.component.scss'
+  styleUrl: './detail-post.component.scss',
 })
-export class DetailPostComponent {
+export class DetailPostComponent implements OnInit {
+  private postService = inject(PostService);
+  private route = inject(ActivatedRoute);
+  public post!: Post | undefined;
+  id: string | null = null;
 
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.post = this.postService.getPostById(Number(this.id));
+  }
 }

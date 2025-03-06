6d51a82be22cd8dd9fa22a750fb9af094ac69455
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-03-06 17:22:40 +0300

    feat(token): added api token

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { API_URL } from '@core/api.token';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
+        provideHttpClient(),
+        { provide: API_URL, useValue: 'http://localhost:5000' }
+    ]
 };

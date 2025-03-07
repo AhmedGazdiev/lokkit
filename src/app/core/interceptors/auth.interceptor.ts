38df817d2f9a30cccf567f70936aac112215c9ce
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-03-07 17:51:01 +0300

    fix(interceptor): fixed auth interceptor

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,7 +1,10 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { LocalStorageService } from '@core/services/local-storage.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-    const token = '';
+    const localStorageService = inject(LocalStorageService);
+    const token = localStorageService.get('token');
     req = req.clone({
         setHeaders: {
             Authorization: `Bearer ${token}`

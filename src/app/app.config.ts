18c3c4cfaa357e57c03df27585eab97a0cebb1f8
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-03-08 14:34:36 +0300

    feat(interceptor): added interceptor for errors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { API_URL } from '@core/api.token';
 import { authInterceptor } from '@core/interceptors/auth.interceptor';
+import { ErrorsInterceptor } from '@core/interceptors/errors.interceptor';
 import { LogginingInterceptor } from '@core/interceptors/logging.interceptor';
 import { environment } from '@environments/environment.dev';
 import { routes } from './app.routes';
@@ -13,6 +14,7 @@ export const appConfig: ApplicationConfig = {
         provideRouter(routes),
         provideHttpClient(withInterceptors([authInterceptor]), withInterceptorsFromDi()),
         { provide: API_URL, useValue: environment.API_URL },
-        { provide: HTTP_INTERCEPTORS, useClass: LogginingInterceptor, multi: true }
+        { provide: HTTP_INTERCEPTORS, useClass: LogginingInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorsInterceptor, multi: true }
     ]
 };

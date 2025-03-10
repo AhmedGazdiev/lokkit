1a358e11a0e3837e6caa8816246c2b2477c11bd1
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-03-10 23:52:11 +0300

    fix(user): removed updateUser method

--- src/app/features/User/pages/users/users.component.ts
+++ src/app/features/User/pages/users/users.component.ts
@@ -1,6 +1,5 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { User } from '@core/models/user';
 import { UserService } from '@core/services/user.service';
 import { UserComponent } from '@features/user/components/user/user.component';
 
@@ -16,21 +15,5 @@ export class UsersComponent implements OnInit {
 
     ngOnInit(): void {
         this.userService.getUsers().subscribe();
-    }
-
-    updateUser(id: number): void {
-        const updatedUser: User = {
-            id: id,
-            fullName: 'Usman Gazdiev',
-            username: '@usm_gaz',
-            email: 'usman@mail.com',
-            avatar: 'https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png',
-            gender: 'male',
-            role: 'user',
-            city: 'Moscow',
-            story: 'I love coding and exploring new technologies.'
-        };
-
-        this.userService.updateUser(id, updatedUser);
     }
 }

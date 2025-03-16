159e37888ceec4e9557be29326d53e2f30be67a6
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-03-16 16:36:04 +0300

    feat(guard): added auth guards

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '@core/services/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (!authService.isLoggedIn.value) {
+    router.navigate(['/login']);
+    return false;
+  }
+  return true;
+};
+
+export const notAuthGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn.value) {
+    router.navigate(['/']);
+    return false;
+  }
+  return true;
+};

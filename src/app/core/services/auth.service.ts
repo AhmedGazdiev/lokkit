d329bfa32a4e709c898ed66e58b2b2cd853fab70
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-03-16 16:05:57 +0300

    feat(auth): added auth status

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  public isLoggedIn = new BehaviorSubject<boolean>(false);
+}

dd46e62894a3fc6c38f760874c8ada3ecf77c417
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-03-18 14:54:34 +0300

    feat(user): added user model

---
+++ src/app/core/models/user.ts
@@ -0,0 +1,20 @@
+export interface User {
+    address: string;
+    avatar: string;
+    createdAt: string;
+    email: string;
+    followers: User[];
+    following: User[];
+    fullname: string;
+    gender: string;
+    mobile: string;
+    password: string;
+    role: string;
+    saved: string[];
+    story: string;
+    updatedAt: string;
+    username: string;
+    website: string;
+    __v: number;
+    _id: string;
+}

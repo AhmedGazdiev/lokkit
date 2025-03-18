6a24d473f07dd3f5a79ea6417ad4ddde5f6859a2
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-03-18 17:23:58 +0300

    feat(auth): added login form validators

--- src/app/features/auth/components/login-form/login-form.component.ts
+++ src/app/features/auth/components/login-form/login-form.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
+import { email, maxLength, minLength, required } from '@shared/validators';
 
 @Component({
     selector: 'login-form',
@@ -14,9 +15,16 @@ export class LoginFormComponent {
     private fb = inject(FormBuilder);
 
     public loginForm = this.fb.group({
-        email: this.fb.control('', []),
-        password: this.fb.control('', [])
+        email: this.fb.control('', [required, email]),
+        password: this.fb.control('', [required, minLength(6), maxLength(16)])
     });
+
+    get emailControl() {
+        return this.loginForm.get(['email']);
+    }
+    get passwordControl() {
+        return this.loginForm.get(['password']);
+    }
 
     onSubmit() {
         if (this.loginForm.valid) {

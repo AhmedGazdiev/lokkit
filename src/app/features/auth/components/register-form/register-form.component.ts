da6fa76d5c75d265258c00f58780ee42c54d01e9
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-03-18 11:51:21 +0300

    feat(required): added custom validator

--- src/app/features/auth/components/register-form/register-form.component.ts
+++ src/app/features/auth/components/register-form/register-form.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
+import { required } from '@shared/validators';
 
 @Component({
   selector: 'register-form',
@@ -12,12 +13,28 @@ import { MatInput } from '@angular/material/input';
 })
 export class RegisterFormComponent {
   public registerForm = new FormGroup({
-    fullname: new FormControl(''),
-    username: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
+    fullname: new FormControl('', required),
+    username: new FormControl('', required),
+    email: new FormControl('', required),
+    password: new FormControl('', required),
+    confirmPassword: new FormControl('', required),
   });
+
+  get fullnameControl() {
+    return this.registerForm.get(['fullname']);
+  }
+  get usernameControl() {
+    return this.registerForm.get(['username']);
+  }
+  get emailControl() {
+    return this.registerForm.get(['email']);
+  }
+  get passwordControl() {
+    return this.registerForm.get(['password']);
+  }
+  get confirmPasswordControl() {
+    return this.registerForm.get(['confirmPassword']);
+  }
 
   onSubmit() {
     if (this.registerForm.valid) {

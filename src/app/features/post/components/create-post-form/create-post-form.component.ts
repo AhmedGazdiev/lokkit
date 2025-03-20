8f92846fe594366db4a8cfca9c43f375a49fed36
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-03-20 17:16:46 +0300

    feat(create-post): added base layout

--- src/app/features/post/components/create-post-form/create-post-form.component.ts
+++ src/app/features/post/components/create-post-form/create-post-form.component.ts
@@ -1,8 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { IconComponent } from '../../../../shared/components/icon/icon.component';
 
 @Component({
     selector: 'create-post-form',
-    imports: [],
+    imports: [IconComponent, MatButton, MatFormField, MatLabel, MatInput],
     templateUrl: './create-post-form.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })

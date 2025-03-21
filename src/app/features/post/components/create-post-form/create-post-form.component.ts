cfb37417a78a98abc1d37a7fbdc800b019510b10
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-03-21 04:42:00 +0300

    feat(create-post): added post images

--- src/app/features/post/components/create-post-form/create-post-form.component.ts
+++ src/app/features/post/components/create-post-form/create-post-form.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
@@ -16,9 +16,33 @@ export class CreatePostFormComponent {
     private fb = inject(FormBuilder);
 
     public createPostForm = this.fb.group({
-        content: this.fb.control(''),
-        images: this.fb.array([])
+        content: this.fb.control('', [required, minLength(20), maxLength(200)]),
+        images: this.fb.array([], required)
     });
+
+    get images() {
+        return this.createPostForm.get(['images']) as FormArray;
+    }
+
+    public removeImage(index: number) {
+        this.images.removeAt(index);
+    }
+
+    public fileUrl(index: number) {
+        const file = this.images.at(index).value;
+        return URL.createObjectURL(file);
+    }
+
+    public changeImages(event: Event) {
+        const inputFiles = event.target as HTMLInputElement;
+        if (inputFiles.files) {
+            const files = Array.from(inputFiles.files);
+            files.forEach(file => {
+                this.images.push(this.fb.control(file));
+            });
+            console.log(files);
+        }
+    }
 
     public onSubmit() {
         if (this.createPostForm.valid) {

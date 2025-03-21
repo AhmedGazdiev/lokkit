4cb8214e159fd0c4514ed54dc1480a6561cbbb32
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-03-21 17:34:43 +0300

    feat(create-post): added post models

---
+++ src/app/core/models/post.ts
@@ -0,0 +1,24 @@
+import { User } from './user';
+
+export interface Post {
+    images: string[];
+    likes: [];
+    comments: [];
+    _id: string;
+    content: string;
+    user: User;
+    createdAt: string;
+    updatedAt: string;
+    __v: 0;
+}
+
+export interface PostResponse {
+    msg: string;
+    post: Post;
+}
+
+export interface GetPostsResponse {
+    msg: string;
+    result: number;
+    posts: Post[];
+}

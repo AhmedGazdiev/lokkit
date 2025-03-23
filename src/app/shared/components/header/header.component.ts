e4114ff809f93298bd6f8ce7387ca312683f04ad
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-03-23 18:20:41 +0300

    fix(drop-down): removed drop-down and added mat-metu from angular-material

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,15 +2,15 @@ import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/cor
 import { MatButton } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '@core/services';
-import { DropDownComponent } from '../drop-down/drop-down.component';
-import { DropDownItem } from '../drop-down/drop-down.type';
+import { DropDownItem } from '@shared/drop-down.type';
 import { IconComponent } from '../icon/icon.component';
 
 @Component({
     selector: 'app-header',
-    imports: [IconComponent, MatFormField, MatLabel, MatInput, MatButton, DropDownComponent, RouterLink],
+    imports: [IconComponent, MatFormField, MatLabel, MatInput, MatButton, RouterLink, MatMenuModule],
     templateUrl: './header.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })

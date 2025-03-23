f4e87d3856a0a217c220f18b32060bf01eb32ea8
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-03-23 23:05:05 +0300

    feat(post): post component

--- src/app/features/post/components/post/post.component.ts
+++ src/app/features/post/components/post/post.component.ts
@@ -1,12 +1,23 @@
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, signal } from '@angular/core';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 import { Post } from '@core/models/post';
+import { CarouselComponent } from '@shared/components';
+import { ShowIfLikedDirective } from '@shared/directives';
+import { UsernamePipe } from '@shared/pipes';
+import { IconComponent } from '../../../../shared/components/icon/icon.component';
 
 @Component({
     selector: 'post',
-    imports: [],
+    imports: [CarouselComponent, IconComponent, UsernamePipe, ShowIfLikedDirective, MatFormField, MatLabel, MatInput],
     templateUrl: './post.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PostComponent {
     public readonly post = input.required<Post>({ alias: 'post' });
+    public _showComments = signal<boolean>(false);
+    public toggleComments() {
+        this._showComments.set(!this._showComments());
+        console.log(this._showComments());
+    }
 }

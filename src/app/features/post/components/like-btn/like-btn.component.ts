f7f507aa501079393c29b8bbba3f79aa8cd37079
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-03-27 17:33:13 +0300

    feat(state): like state

--- src/app/features/post/components/like-btn/like-btn.component.ts
+++ src/app/features/post/components/like-btn/like-btn.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, OnInit, signal } from '@angular/core';
+import { Post } from '@core/models/post';
+import { AuthService } from '@core/services';
 import { PostService } from '@core/services/post.service';
 import { IconComponent } from '@shared/components';
 
@@ -8,14 +10,22 @@ import { IconComponent } from '@shared/components';
     templateUrl: './like-btn.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LikeBtnComponent {
-    public readonly id = input.required<string>();
+export class LikeBtnComponent implements OnInit {
+    public readonly post = input.required<Post>();
     public readonly postService = inject(PostService);
+    public readonly authService = inject(AuthService);
+    public _isLike = signal<boolean>(false);
+
+    ngOnInit(): void {
+        if (this.post().likes.find(i => i._id === this.authService.authData()?._id)) {
+            this._isLike.set(true);
+        }
+    }
 
     like() {
-        this.postService.likePost(this.id()).subscribe();
+        this.postService.likePost(this.post()._id).subscribe();
     }
     unlike() {
-        this.postService.unlikePost(this.id()).subscribe();
+        this.postService.unlikePost(this.post()._id).subscribe();
     }
 }
